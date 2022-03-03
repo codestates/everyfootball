6549db6b6fba3d4a4d340359c85d003cdf24b243
EuilimChoi <91925895+EuilimChoi@users.noreply.github.com>
2022-03-03 22:56:25 +0900

    로그인, 유저 정보 요청 작성

--- server/controllers/user/signup.js
+++ server/controllers/user/signup.js
@@ -23,17 +23,11 @@ sequelize.sync().then((result)=> {
         location : req.body.location,
         position : req.body.position})
     }
+).then(
+    res.status(200).json({"message": "sign up done"})
 ).catch((err)=> {
     console.log(err)
 })
-
-
-
-res.status(200).json({message : "work!", "payload" : payload})
-
-
-
-
 
 }
 
