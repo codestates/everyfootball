bce30fc310f683378176fc3f85a1ec611ff73a24
hyukzz <ri_chard@naver.com>
2022-03-06 21:01:05 +0900

    userinfo

---
+++ client/src/Components/Userinfo/UserDelete.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import React from "react";
+import "./UserDelete.css";
+
+const userDelete = ({ setLogin }) => {
+    axios
+        .delete("http://localhost:4000/", {
+            // headers:{localStorage.getItem("accessToken")}
+            headers: { "Content-Type": "application/json" },
+        })
+        .then((res) => {
+            setLogin(false);
+            console.log("탈퇴");
+            alert("회원탈퇴가 완료되었습니다.");
+            window.location.replace("/");
+        })
+        .catch((err) => {
+            console.log("회원탈퇴 에러", err);
+            // alert("잘못된 요청입니다");
+        });
+
+    return (
+        <>
+            <button className="delete" onClick={userDelete}>
+                회원 탈퇴
+            </button>
+        </>
+    );
+};
+
+export default userDelete;
