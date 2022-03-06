a73bce4891149821f92d24505047a4863de125c4
rkdghwnd <bsc6448@naver.com>
2022-03-07 00:03:52 +0900

    헤더 푸터 회원가입 완성

--- client/src/Components/RecordBoardPage/RecordBoard.js
+++ client/src/Components/RecordBoardPage/RecordBoard.js
@@ -1,24 +1,21 @@
-import React, { useState } from 'react';
-import Rank from './Rank';
-import Board from './Board';
-import './RecordBoard.css';
-import Header from '../Mainpage/Header'
-import Footer from '../Mainpage/Footer'
+import React, { useState } from "react";
+import Rank from "./Rank";
+import Board from "./Board";
+import "./RecordBoard.css";
+import Header from "../Mainpage/Header";
+// import Footer from '../Mainpage/Footer'
 // import { Link } from 'react-router-dom';
 // import axios from 'axios';
 
 //axios.defaults.withCredentials = true;
 
-export default function RecordBoard () {
-
-
-  
-  return (
-    <div>
-      {/* <Header /> */}
-      <Rank />
-      <Board />
-      {/* <Footer /> */}
-    </div>
-  );
+export default function RecordBoard() {
+    return (
+        <div>
+            {/* <Header /> */}
+            <Rank />
+            <Board />
+            {/* <Footer /> */}
+        </div>
+    );
 }
