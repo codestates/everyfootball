1be954820d9a7a302b73642e3c77b9a9875ae22f
jin <jin921207@naver.com>
2022-03-08 00:08:16 +0900

    예약하기 버튼으로 데이터를 넘겨줌

--- client/src/Components/Mainpage/Match.js
+++ client/src/Components/Mainpage/Match.js
@@ -1,16 +1,22 @@
 import react from "react";
 import "./Match.css";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 const Match = ({ date, location, gender, headCount }) => {
+    const history = useHistory();
+
+    const clickReserved = () => {
+        history.push("/matchinfo", {state:{date}})
+    }
+
     return (
         <div className="row">
             <div className="col">⏲️ {date}</div>
             <div className="col">🏟️ {location}</div>
             <div className="col">{gender}</div>
             <div className="col">{headCount}</div>
-            <div className="col">
-                <Link to="/matchinfo">예약하기</Link>
+            <div className="col" onClick={clickReserved}>
+                예약하기
             </div>
         </div>
     );
