7c040350cf59e9ba4fd07fc33a4f936365a5bbdc
jin <jin921207@naver.com>
2022-03-08 00:01:29 +0900

    migration 수정

--- server/controllers/user/signup.js
+++ server/controllers/user/signup.js
@@ -4,7 +4,6 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res) => {
   console.log("signup");
 
-<<<<<<< HEAD
   let payload = {
     userid: req.body.userid,
     password: req.body.password,
@@ -25,31 +24,6 @@ module.exports = (req, res) => {
         location: req.body.location,
         position: req.body.position,
       });
-=======
-    let payload = {
-        userid : req.body.userid,
-        password : req.body.password,
-        fullname : req.body.fullname,
-        gender : req.body.gender,
-        phonenum : req.body.phonenum,
-        position : req.body.position,
-        preferredtime : req.body.preferredtime,
-        preferredloca : req.body.preferredloca
-    }
-
-    sequelize.sync().then((result)=> {
-    return users.create({
-        userid : req.body.userid,
-        password : req.body.password,
-        fullname : req.body.fullname,
-        gender : req.body.gender,
-        position : req.body.position,
-        preferredtime : req.body.preferredtime,
-        preferredloca : req.body.preferredloca})
-    })
-    .catch((err)=> {
-    console.log(err)
->>>>>>> 2a0ac188798099902ca3ff4980d303ab00c36094
     })
     .catch((err) => {
       console.log(err);
