35593167cf39f78b8756ee78c416184baae18657
jin <jin921207@naver.com>
2022-03-08 22:01:06 +0900

    이미지 불러오기

--- client/src/Components/Mainpage/Match.js
+++ client/src/Components/Mainpage/Match.js
@@ -1,20 +1,20 @@
 import react from "react";
 import "./Match.css";
 import { Link } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 const Match = ({ matchData }) => {
+    const history = useHistory();
     return (
         <div class="match-li">
             <a class="match-link">
                 <div class="match-time">{matchData.time}</div>
                 <div class="match-info">
-                    <h3>{matchData.location}}</h3>
-                    <span>{matchData.matchgender}} </span>
+                    <h3>{matchData.location}</h3>
+                    <span>{matchData.matchgender}</span>
                     <span>6vs6 </span>
                     <span>모든레벨</span>
                 </div>
-                <Link to="matchinfo">
-                    <div class="match-status">마감</div>
-                </Link>
+                <div class="match-status" onClick={() => history.push('/matchinfo', {state : matchData})}>마감</div> 
             </a>
         </div>
     );
