423667387482c728fdac724ee58334547c7e94c9
jin <jin921207@naver.com>
2022-03-08 22:00:44 +0900

    MatchInfo 수정

---
+++ client/src/Components/MatchInfo/StadiumImage.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+// import { Link } from 'react-router-dom';
+// import axios from 'axios';
+
+//axios.defaults.withCredentials = true;
+
+export default function StadiumImage ({matchId}) {
+
+  const [groundData, setGroundData] = useState(null)
+
+   //axios 랑 이 방법의 차이
+   const getGroundData = async(id) => {
+    const response = await axios.get(`http://localhost:4000/match/data/${id}?matchid=${id}`)
+    //console.log(response.data)
+    setGroundData(response.data.data.groundinfo)
+  }
+  //랜더링 될때 한번만 실행
+  useEffect(() => {
+    console.log("matchId",matchId)
+    getGroundData(matchId);
+  }, [])
+
+  const imagestyle = {
+    height: "60vh",
+    width: "80vw",
+  };
+
+  if(groundData === null){
+    return <>로딩중</>
+  }
+  
+  return (
+    <div>
+      <div className='stadiumImage'>
+          <img src={groundData.groundpayload.img} style={imagestyle} alt="futsal"></img>
+      </div>  
+    </div>
+  );
+}
