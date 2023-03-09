4a37fabe1918591237a04bbe68fe459dd7ff131b
youn hyuk <81045794+hyukzz@users.noreply.github.com>
2023-03-10 02:52:51 +0900

    fix: login logic

--- client/src/Components/Login/LoginForm.js
+++ client/src/Components/Login/LoginForm.js
@@ -15,14 +15,13 @@ const LoginForm = () => {
     const onClickLogin = () => {
         const { userid, password } = loginInfo;
         if (userid === "") {
-            console.log("아이디를 입력하세요");
+            alert("아이디를 입력하세요");
             return;
         } else if (password === "") {
-            console.log("비밀번호를 입력하세요");
+            alert("비밀번호를 입력하세요");
             return;
         }
-        console.log("click login");
-        axios
+          axios
             .post(
                 `${process.env.REACT_APP_API_URL}/user/login`,
                 {
@@ -32,32 +31,22 @@ const LoginForm = () => {
                 { "Content-Type": "application/json" },
             )
             .then((res) => {
-                console.log(res);
-                console.log(res.data.data.accessToken);
                 localStorage.setItem("accessToken", res.data.data.accessToken);
                 localStorage.setItem("fullname", res.data.data.fullname);
-                console.log("성공");
+                
                 if (res.data.data.accessToken) {
                     localStorage.setItem("accessToken", res.data.data.accessToken);
-                    console.log("======================", "로그인 성공");
-                    console.log(localStorage);
                 }
-                // 작업 완료 되면 페이지 이동(새로고침)
-                // document.location.href = "/";
+                
                 return window.location.replace("/");
             })
             .catch((error) => {
-                console.log(error);
                 alert("아이디와 비밀번호를 확인해 주세요.");
             });
     };
     return (
         <div className="wrap">
             <div className="login">
-                {/* <div class="login_sns">
-<li><Link to=""><i class="fab fa-kakaotalk"></i></Link></li>
-<li><Link to=""><i class="fab fa-kakaotalk"></i></Link></li>
-</div> */}
                 <div className="login_id">
                     <h4>아이디</h4>
                     <input type="text" placeholder="아이디" name="input_id" onChange={handleInputValue("userid")} />
